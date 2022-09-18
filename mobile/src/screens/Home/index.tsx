ccc009732fadb91f24232d61be310c9785f7c14d
Gabriel Veras <bielveras.e@hotmail.com>
2022-09-17 23:21:37 -0300

    aula 2 finalizado

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,18 +1,34 @@
-import React from 'react';
-import { Image, View } from 'react-native';
+import { Image, View, FlatList } from "react-native";
 
-import logoImg from '../../assets/logo-nlw-esports.png'
+import logoImg from "../../assets/logo-nlw-esports.png";
+import { GameCard } from "../../components/GameCard";
+import { Heading } from "../../components/Heading";
 
-import { styles } from './styles';
+
+import { GAMES } from "../../utils/games";
+import { styles } from "./styles";
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <Image
-        source={logoImg}
-        style={styles.logo}
-
+      <Image source={logoImg} style={styles.logo} />
+      <Heading
+        title="Encontre seu duo!"
+        subtitle="Seleciona o game que deseja jogar..."
       />
+
+    <FlatList data={GAMES}
+    keyExtractor={item => item.id}
+    renderItem={({item}) => (
+      <GameCard 
+      data={item}
+      />
+    )}
+    showsHorizontalScrollIndicator={false}
+    horizontal
+    contentContainerStyle={styles.contentList}
+    />
+
     </View>
   );
 }
