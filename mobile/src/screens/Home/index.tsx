115b76b85503102162a3ada752967ebf645b7b66
Gabriel Veras <bielveras.e@hotmail.com>
2022-09-20 01:56:15 -0300

    Finalizado telas principais

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,27 +1,45 @@
-import { Image, View, FlatList } from "react-native";
-
+import { Image,FlatList } from "react-native";
+import {SafeAreaView} from "react-native-safe-area-context"
 import logoImg from "../../assets/logo-nlw-esports.png";
-import { GameCard } from "../../components/GameCard";
+import { GameCard, GameCardProps } from "../../components/GameCard";
 import { Heading } from "../../components/Heading";
-
-
-import { GAMES } from "../../utils/games";
+import { useNavigation} from '@react-navigation/native'
+import { AppRoutes } from "../../routes/app.routes";
 import { styles } from "./styles";
+import {useEffect, useState} from 'react'
+import { Background } from "../../components/Background";
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([]);
+  const navigation = useNavigation();
+
+  function handleOpenGame({id, title, bannerUrl}: GameCardProps) {
+    navigation.navigate('game', {id, title, bannerUrl});
+  }
+
+
+  useEffect(() => {
+    fetch('http://192.168.1.2:3333/games')
+    .then(response => response.json())
+    .then(data => setGames(data))
+  }, []);
+
   return (
-    <View style={styles.container}>
+    <Background>
+    <SafeAreaView style={styles.container}>
       <Image source={logoImg} style={styles.logo} />
       <Heading
         title="Encontre seu duo!"
         subtitle="Seleciona o game que deseja jogar..."
       />
 
-    <FlatList data={GAMES}
+    <FlatList 
+    data={games}
     keyExtractor={item => item.id}
     renderItem={({item}) => (
       <GameCard 
       data={item}
+      onPress={() => handleOpenGame(item)}
       />
     )}
     showsHorizontalScrollIndicator={false}
@@ -29,6 +47,7 @@ export function Home() {
     contentContainerStyle={styles.contentList}
     />
 
-    </View>
+    </SafeAreaView>
+    </Background>
   );
 }
